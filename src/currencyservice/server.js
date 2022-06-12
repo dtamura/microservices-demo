e07ed6318e3d8e7f2253266b027293f0232022f9
Daiki Tamura <dtamura23@outlook.jp>
2022-06-12 09:44:50 +0000

    currencyservice (Node.js) に dd-traceを追加

--- src/currencyservice/server.js
+++ src/currencyservice/server.js
@@ -34,6 +34,11 @@ if(process.env.DISABLE_TRACING) {
 else {
   console.log("Tracing enabled.")
   require('@google-cloud/trace-agent').start();
+  require("dd-trace").init({
+    tags: {
+      domain: "common",
+    },
+});
 }
 
 if(process.env.DISABLE_DEBUGGER) {
